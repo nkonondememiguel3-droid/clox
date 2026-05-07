d0e5d0f5ef5c734dc5772d798b0d330aee9a46aa
Nkono Ndeme Miguel <nkonondememiguel7@gmail.com>
2026-05-07 10:39:32 +0100

    compiling constants and grouping values

--- compiler.c
+++ compiler.c
@@ -2,15 +2,17 @@
 #include "chunk.h"
 #include "common.h"
 #include "scanner.h"
+#include "value.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
   __token_t__ current;
   __token_t__ previous;
   bool had_error;
   bool panic_mode;
-} __parser_t__ ;
+} __parser_t__;
 
 __parser_t__ parser;
 __chunk_t__ *compiling_chunk;
@@ -21,28 +23,26 @@ static void emit_byte(uint8_t byte) {
   write_chunk(current_chunk(), byte, parser.previous.line);
 }
 
-#if 0
 static void emit_bytes(uint8_t byte_1, uint8_t byte_2) {
   emit_byte(byte_1);
   emit_byte(byte_2);
 }
-#endif
 
-static void emit_return(void) {
-  emit_byte(OP_RETURN);
-}
+static void emit_return(void) { emit_byte(OP_RETURN); }
 
 static void end_compiler(void) { emit_return(); }
 
 static void error_at(__token_t__ *token, const char *message) {
-  if (parser.panic_mode) return;
+  // if we already are in panic mode, we discard the current error
+  if (parser.panic_mode)
+    return;
   parser.panic_mode = true;
 
   fprintf(stderr, "[line %d] Error", token->line);
 
-  if ( token->type == TOKEN_EOF ) {
+  if (token->type == TOKEN_EOF) {
     fprintf(stderr, " at end");
-  } else if ( token->type == TOKEN_ERROR ) {
+  } else if (token->type == TOKEN_ERROR) {
     // nothing.
   } else {
     fprintf(stderr, " at '%.*s'", token->length, token->start);
@@ -56,26 +56,23 @@ static void error_at_current(const char *message) {
   error_at(&parser.current, message);
 }
 
-#if 0
-static void error(const char *message) {
-  error_at(&parser.previous, message);
-}
-#endif
+static void error(const char *message) { error_at(&parser.previous, message); }
 
 static void advance(void) {
 
   parser.previous = parser.current;
 
-  for ( ;; ) {
+  for (;;) {
     parser.current = scan_token();
-    if (parser.current.type != TOKEN_ERROR) break;
+    if (parser.current.type != TOKEN_ERROR)
+      break;
 
     error_at_current(parser.current.start);
   }
 }
 
 static void consume(__token_type_t__ token_type, const char *message) {
-  if ( parser.current.type == token_type ) {
+  if (parser.current.type == token_type) {
     advance();
     return;
   }
@@ -83,8 +80,31 @@ static void consume(__token_type_t__ token_type, const char *message) {
   error_at_current(message);
 }
 
-static void expression(void) {
+static uint8_t make_constant(__value_t__ constant) {
 
+  int constant_idx = add_constant(compiling_chunk, constant);
+  if (constant_idx > UINT8_MAX) {
+    error("Too many constants in a chunk.");
+    return 0;
+  }
+
+  return (uint8_t)constant;
+}
+
+static void emit_constant(__value_t__ constant) {
+  emit_bytes(OP_CONSTANT, make_constant(constant));
+}
+
+static void expression(void) {}
+
+static void grouping(void) {
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number(void) {
+  double value = strtod(parser.previous.start, NULL);
+  emit_constant(value);
 }
 
 bool compile(const char *source, __chunk_t__ *chunk) {
